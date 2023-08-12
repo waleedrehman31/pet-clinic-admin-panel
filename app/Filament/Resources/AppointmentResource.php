4f0b46bf359ca8b55dcfe1baac0c03ab9b8581ca
waleedrehman31 <waleedrehman31@gmail.com>
2023-08-12 20:16:27 +0500

    create and complete appointment filament resource

---
+++ app/Filament/Resources/AppointmentResource.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AppointmentResource\Pages;
+use App\Filament\Resources\AppointmentResource\RelationManagers;
+use App\Models\Appointment;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AppointmentResource extends Resource
+{
+    protected static ?string $model = Appointment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make(
+                    [
+                        Forms\Components\DatePicker::make('date')
+                            ->required()
+                            ->native(false),
+                        Forms\Components\TimePicker::make('start')
+                            ->required()
+                            ->secionds(false)
+                            ->displayformate('h:i A'),
+                        Forms\Components\TimePicker::make('end')
+                            ->required()
+                            ->secionds(false)
+                            ->displayformate('h:i A'),
+                        Forms\Components\Select::make('pet_id')
+                            ->required()
+                            ->relationship('pet', 'name')
+                            ->searchable()
+                            ->preload(),
+                        Forms\Components\TextInput::make('description')
+                            ->required()
+                    ]
+                )
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('pet.name')
+                    ->seachable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->seachable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('date')
+                    ->date('M d Y')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('start')
+                    ->time('h:i A')
+                    ->label('From')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('end')
+                    ->time('h:i A')
+                    ->label('To')
+                    ->sortable(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAppointments::route('/'),
+            'create' => Pages\CreateAppointment::route('/create'),
+            'edit' => Pages\EditAppointment::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/AppointmentResource/Pages/EditAppointment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\AppointmentResource\Pages;
+
+use App\Filament\Resources\AppointmentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditAppointment extends EditRecord
+{
+    protected static string $resource = AppointmentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    public function getRedirectUrl(): ?string {
+        return $this->getResource()::getUrl('index');
+    }
+}
