77f3e050dfad37201fde697a3af0ab1c26d2aa0e
waleedrehman31 <waleedrehman31@gmail.com>
2023-08-13 12:05:03 +0500

    add colors to appointmentstatus enum

--- app/Enums/AppointmentStatus.php
+++ app/Enums/AppointmentStatus.php
@@ -2,9 +2,10 @@
 
 namespace App\Enums;
 
+use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasLabel;
 
-enum AppointmentStatus: string implements HasLabel
+enum AppointmentStatus: string implements HasLabel, HasColor
 {
   case Created = 'created';
   case Confirmed = 'confirmed';
@@ -16,6 +17,14 @@ enum AppointmentStatus: string implements HasLabel
       self::Created => 'Created',
       self::Confirmed => 'Confirmed',
       self::Canceled => 'Canceled',
+    };
+  }
+  public function getColor(): string|array|null
+  {
+    return match ($this) {
+      self::Created => 'warning',
+      self::Confirmed => 'success',
+      self::Canceled => 'danger'
     };
   }
 }
