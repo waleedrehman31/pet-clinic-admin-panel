24f127621b33a6f1ce62d0437fb4af1fa862b7ae
waleedrehman31 <waleedrehman31@gmail.com>
2023-09-10 12:27:20 +0500

    create user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()
+            ->create(
+                [
+                    'role_id' => Role::whereName('admin')->first()->id,
+                    'phone' => '031111111'
+                ]
+            );
+    }
+}
