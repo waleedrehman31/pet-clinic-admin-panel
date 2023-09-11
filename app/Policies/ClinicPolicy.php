be4b5c713a1ad2d3fb088b4f64ee0e98ad0d0489
waleedrehman31 <waleedrehman31@gmail.com>
2023-09-11 22:21:34 +0500

    create clinic policy

---
+++ app/Policies/ClinicPolicy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class ClinicPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function create()
+    {
+        return auth()->user()->role->name == 'admin';
+    }
+}
